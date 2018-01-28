9a6fc99f9bd48922970793424d712cc60eb6a18a
JeremyWeed <jweed262@gmail.com>
2018-01-28 18:08:49 -0500

    finished, not compiled

--- led_controller/include/outrun.hpp
+++ led_controller/include/outrun.hpp
@@ -5,10 +5,13 @@
 #include "led.hpp"
 
 #define OUTRUN_NODES 5
+#define NUM_COLORS 4
 
 typedef struct outrun_struct {
 	Color colors[4];
-
+	long length;
+	int node_locations[OUTRUN_NODES];
+	Color node_colors[OUTRUN_NODES];
 } OutrunSet;
 
 extern OutrunSet *outrun_set;
--- led_controller/outrun.cpp
+++ led_controller/outrun.cpp
@@ -10,13 +10,50 @@ void outrun_init() {
 	OutrunSet *os = outrun_set;
 
 	setCol(&os->cols[0], 0x07d8cf); // blue
-	setCol(&os->cols[1], 0xea0ab3); //pink
-	setCol(&os->cols[2], 0xf7a481); //orange/yellow
+	setCol(&os->cols[1], 0xea0ab3); // pink
+	setCol(&os->cols[2], 0xf7a481); // orange/yellow
 	setCol(&os->cols[3], 0x170562); // dark blue
+
+	os->length = 10000; // ms
+
+	unsigned char step = LENGTH / (OUTRUN_NODES - 1);
+	for (int i = 0; i < OUTRUN_NODES - 1; i++){
+		os->node_locations[i] = i * step;
+	}
+	// Doing this to watch out for integer division errors
+	os->node_locations[OUTRUN_NODES - 1] = LENGTH - 1;
+
+	for (int i = 0; i < OUTRUN_NODES; i++) {
+		os->node_colors[i] = os->cols[0];
+	}
 }
 
 void drive() {
+	static unsigned long last_loop = 0;
+	last_loop = millis();
 
+	// Change the color of one node
+	os->node_colors[random(OUTRUN_NODES)] = cols[random(NUM_COLORS)];
+
+	unsigned char node = 1;
+	for (int i = 0; i < LENGTH; i++) {
+		if (i == os->node_locations[node]) {
+			leds.setPixelColor(i,
+							   os->node_colors[node].R,
+							   os->node_colors[node].G,
+							   os->node_colors[node].B);
+			node += 1;
+		} else {
+			Color col = combine(os->node_colors[node - 1], os->node_colors[node],
+								i - os->node_locations[node - 1],
+								os->node_locations[node] - i);
+			leds.setPixelColor(i, col.R, col.G, col.B);
+		}
+	}
+	if (last_loop + outrun_set->length > millis()){
+		delay(last_loop - millis() + outrun_set->length);
+	}
+	leds.show();
 }
 
 static Color combine(Color one, Color two, int dist_one, int dist_two) {
